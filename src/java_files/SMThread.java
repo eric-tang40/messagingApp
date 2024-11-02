80b631d67d25046ce29e17fce449b6b12a4fea7e
Aneesh Dake <adake@purdue.edu>
2024-11-02 15:15:18 -0400

    Update SMThread.java

--- src/java_files/SMThread.java
+++ src/java_files/SMThread.java
@@ -1,25 +1,30 @@
-//public class SMThread extends Thread {
-//
-//    public void run() {
-//        System.out.println("Running!");
-//    }
-//
-//    public static void main(String[] args) {
-//
-//        SMThread toRun = new SMThread();
-//        toRun.start();
-//
-//        try {
-//
-//            toRun.join();
-//
-//        } catch (InterruptedException ie) {
-//
-//            ie.printStackTrace
-//
-//        }
-//
-//    }
-//
-//
-//}
+package java_files;
+
+public class SMThread implements Runnable {
+    private String message;
+    private boolean isSending;
+
+    public SMThread(String message, boolean isSending) {
+        this.message = message;
+        this.isSending = isSending;
+    }
+
+    @Override
+    public void run() {
+        if (isSending) {
+            sendMessage();
+        } else {
+            receiveMessage();
+        }
+    }
+
+    private void sendMessage() {
+        // Code to send the message
+        System.out.println("Sending: " + message);
+    }
+
+    private void receiveMessage() {
+        // Code to receive messages (simulate for demo)
+        System.out.println("Receiving messages...");
+    }
+}
